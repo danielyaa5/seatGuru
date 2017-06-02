d7119af3320509ec4d72eb0877606c1afe07a5fb
Emrullah Karatas <emrullah@efabrika.com>
2017-06-02 14:45:50 +0300

    added download images

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const async = require('async');
+const download = require('image-downloader');
+const fs = require('fs');
 
 const HomeUrl = 'https://www.seatguru.com';
 const browseAirlinesUrl = 'https://www.seatguru.com/browseairlines/browseairlines.php';
 
 class SeatGuru {
+  constructor() {
+    this.dir = './images/';
+    if (!fs.existsSync(this.dir)) {
+      fs.mkdirSync(this.dir);
+    }
+  }
 
   // set All Airlines Urls
   setRequestUrls(data) {
     return new Promise((resolve) => {
-      this.MainPageCheerio(data)
+      this.mainPageCheerio(data)
       .then((response) => {
         resolve(response);
       });
@@ -27,18 +35,29 @@ class SeatGuru {
   }
 
   // run All url's request
-  AllRequest(urls) {
+  allRequest(urls) {
     return new Promise((resolve, reject) => {
       const arr = [];
       const limit = 4;
       async.eachLimit(urls, limit, (file, cb) => {
         this.request(HomeUrl + file)
         .then((data) => {
-          this.EachPageCheerio(data)
+          this.eachPageCheerio(data)
           .then((obj) => {
             console.log(obj);
-            arr.push(obj);
-            cb();
+
+            // download images to local
+            const url = obj.imgUrl;
+            const name = obj.airlineName.replace(' ', '_');
+            const shortname = obj.imgUrl.split('/')[8];
+            const dest = this.dir + name + '_' + shortname;
+            this.download(url, dest)
+            .then(() => {
+              const lastObj = obj;
+              lastObj.localUrl = dest;
+              arr.push(lastObj);
+              cb();
+            });
           });
         });
       }, (err) => {
@@ -48,8 +67,21 @@ class SeatGuru {
     });
   }
 
+
+  download(url, dest) {
+    return new Promise((resolve, reject) => {
+      const options = { url, dest };
+      download.image(options)
+        .then(({ filename, image }) => {
+          console.log('File saved to', filename);
+          resolve();
+        }).catch((err) => {
+          throw err;
+        });
+    });
+  }
   // Main Page Selector (for get airlines url)
-  MainPageCheerio(data) {
+  mainPageCheerio(data) {
     return new Promise((resolve, reject) => {
       const $ = cheerio.load(data);
       const arr = [];
@@ -62,12 +94,12 @@ class SeatGuru {
   }
 
   // Each Airline Page Selector (for get each airline's information)
-  EachPageCheerio(data) {
+  eachPageCheerio(data) {
     return new Promise((resolve, reject) => {
       const $ = cheerio.load(data);
       const imgUrl = $('img', '.airlineBannerLargeLeft').attr('src');
       const airlineCode = $('.ai-info', '.airlineBannerLargeRight').html();
-      const airlineName = $('h1', '.title').text().split('(')[0];
+      const airlineName = $('h1', '.title').text().split('(')[0].trim();
       resolve({ imgUrl, airlineCode, airlineName });
     });
   }
@@ -76,6 +108,6 @@ class SeatGuru {
 const obj = new SeatGuru();
 obj.request(browseAirlinesUrl)
 .then(data => obj.setRequestUrls(data))
-.then(urls => obj.AllRequest(urls))
+.then(urls => obj.allRequest(urls))
 .then(console.log)
 .catch(console.log);
